2305414d30d8498c9ba7f5bdf2ffdf8376bf7c4d
unknown <joshrde2002@gmail.com>
2025-01-25 20:44:55 +0300

    Form Layout

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -9,6 +9,18 @@ import {
   FieldValues,
 } from "react-hook-form";
 import { ZodType } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "./ui/input";
+import Link from "next/link";
 
 interface Props<T extends FieldValues> {
   schema: ZodType<T>;
@@ -23,6 +35,7 @@ const AuthForm = <T extends FieldValues>({
   defaultValues,
   onSubmit,
 }: Props<T>) => {
+  const isSignIn = type === "SIGN_IN";
   const form: UseFormReturn<T> = useForm({
     resolver: zodResolver(schema),
     defaultValues: defaultValues as DefaultValues<T>,
@@ -32,7 +45,48 @@ const AuthForm = <T extends FieldValues>({
     onSubmit(data);
     console.log(data);
   };
-  return <div>AuthForm {type}</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-semibold text-white">
+        {isSignIn ? "Welcome Back To Bookwise" : "Create Your Account With Us"}
+      </h1>
+      <p className="text-light-100">
+        {isSignIn
+          ? "Access the vast collection of resources, and stay updated"
+          : "please complete all the fields and upload a valid university ID to gain access"}
+      </p>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+      <p className="text-center text-base font-medium">
+        {isSignIn ? "New to Bookwise ? " : "Already have an account ? "}
+        <Link
+          href={isSignIn ? "/signup" : "/signin"}
+          className="font-bold text-primary"
+        >
+          {isSignIn ? "Create an account" : "Sign in"}
+        </Link>
+      </p>
+    </div>
+  );
 };
 
 export default AuthForm;
