3810df4cc4833dddeefa2b526933d81b3deafc33
unknown <joshrde2002@gmail.com>
2025-01-27 20:29:40 +0300

    dotenv

--- src/drizzle.config.ts
+++ src/drizzle.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
 
+config({ path: ".env.local" });
 export default defineConfig({
   schema: "./src/database/schema.ts",
   out: "./migrations",
