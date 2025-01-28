ba28654397db65dc07768fae8cd19a67a2c6fcdf
unknown <joshrde2002@gmail.com>
2025-01-28 11:25:53 +0300

    Profile Page

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,12 +1,14 @@
+import { auth } from "@/auth";
 import Header from "@/components/Header";
 
 import React from "react";
 
-function Layout({ children }: { children: React.ReactNode }) {
+async function Layout({ children }: { children: React.ReactNode }) {
+  const session = await auth();
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl text-center">
-        <Header />
+        <Header session={session} />
         <div className="mt-20 pb-20">{children}</div>
       </div>
     </main>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,20 @@
 "use client";
-import { cn } from "@/lib/utils";
+import { cn, getInitialNameFromSession } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
-
-export default function Header() {
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+interface Session {
+  user?: {
+    email?: string;
+    id?: string;
+  };
+  expires?: string;
+}
+export default function Header({ session }: { session: Session }) {
   const path = usePathname();
+  console.log(getInitialNameFromSession(session));
+  console.log(session);
   return (
     <header className="my-10 flex justify-between gap-5">
       <Link href="/">
@@ -22,6 +30,16 @@ export default function Header() {
             )}
           >
             Library
+          </Link>
+        </li>
+        <li>
+          <Link href="profile">
+            <Avatar>
+              {/* <AvatarImage src="https://github.com/shadcn.png" /> */}
+              <AvatarFallback className="bg-amber-100 text-gray-500">
+                {getInitialNameFromSession(session)}
+              </AvatarFallback>
+            </Avatar>
           </Link>
         </li>
         <li>
