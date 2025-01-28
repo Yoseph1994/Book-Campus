6b823c63fcf32d6bd3ccf39a05c811f86357a98a
unknown <joshrde2002@gmail.com>
2025-01-28 12:01:47 +0300

    Fixed Git Conflict

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,17 +2,10 @@ import { auth } from "@/auth";
 import Header from "@/components/Header";
 import { redirect } from "next/navigation";
 
-<<<<<<< HEAD
 async function Layout({ children }: { children: React.ReactNode }) {
   const session = await auth();
   if (!session) redirect("/signin");
-=======
-import React from "react";
 
-async function Layout({ children }: { children: React.ReactNode }) {
-  const session = await auth();
-  if (!session) redirect("/sigin");
->>>>>>> ba6e8db5c2c8cc2a0e594bd1287fd3535fddb94d
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl text-center">
