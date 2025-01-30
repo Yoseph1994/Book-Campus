2940d5f962b1d16243369a06338b311437aa1a82
unknown <joshrde2002@gmail.com>
2025-01-30 10:17:31 +0300

    Prod Url in config

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ export const config = {
   env: {
     databaseUrl: process.env.DATABASE_URL!,
     apiEndPoint: process.env.NEXT_PUBLIC_API_URL!,
+    apiProdEndPoint: process.env.NEXT_PUBLIC_PROD_API_URL!,
     imageKit: {
       publicKey: process.env.NEXT_PUBLIC_KEY!,
       privateKey: process.env.IMAGE_KIT_PRIVATE_KEY!,
